0824e3152e62519c2c2994550d460f9dca70ecb5
Mario Godinez Medina <futureweb2000@gmail.com>
2016-11-24 22:34:47 -0600

    New sections and layout basic

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var gulp = require('gulp'),
 		connect.server({
 			root: './app',
 			hostname: '0.0.0.0',
-			port: 8080,
+			port: 8081,
 			livereload: true
 			// middleware: function(connect, opt) {
 		 //       return [ historyApiFallback ];
